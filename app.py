b4006c053d6982d40646da89da410009922a7cb6
WaeniKakenyi <medrinewaeni@gmail.com>
2023-01-17 15:03:43 +0300

    Issues again

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 import replicate
 import os
 
-os.environ["REPLICATE_API_TOKEN"] = '0aa7780cda0b711d75de41356b5ac4703f0d'
+os.environ["REPLICATE_API_TOKEN"] = '6e020aa7780cda0b711d75de41356b5ac4703f0d'
 
 model = replicate.models.get("yuval-alaluf/sam")
 version = model.versions.get("9222a21c181b707209ef12b5e0d7e94c994b58f01c7b2fec075d2e892362f13c")
